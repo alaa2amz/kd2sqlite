c3d8a778de45cbd585fb326271414069a6ebd3e9
alaa zakaria <alaa2amz@gmail.com>
2018-05-25 01:54:20 +0200

    first commit to kanjidic2.xml to sqlite converter
    using python

---
+++ kanjidic2_schema.sql
@@ -0,0 +1,300 @@
+
+--foreign key (house_id) references houses(id),
+create table header
+    (
+    file_version text,
+    database_version text,
+    date_of_creation text,
+    date_of_generation text,
+    input_file_name text
+    );
+    
+create table character --mainkanjitable.
+	(
+	ch_id integer,
+	literal text,
+
+	primary key (ch_id, literal)
+	);
+
+create table cp_value --encodings.
+	(
+	ch_id integer,
+	cp_value text,
+	cp_type_id integer,
+
+	foreign key (ch_id) references character(ch_id),
+	foreign key (cp_type_id) references cp_type(cp_type_id),
+	primary key (ch_id, cp_value, cp_type_id)
+	);
+
+--/meta
+create table cp_type --code point type.
+	(
+	cp_type_id integer,
+	cp_type_name text,
+
+	primary key (cp_type_id, cp_type_name)
+	);
+
+create table rad_value --bushu or radical number.
+	(
+	ch_id integer,
+	rad_value integer,
+	rad_type_id integer,
+
+	foreign key (ch_id) references character(ch_id),
+	foreign key (rad_type_id) references rad_type(rad_type_id),
+	primary key (ch_id, rad_value, rad_type_id)
+	);
+
+--/meta
+create table rad_type --KangXi Zidian. or nelson_c.
+	(
+	rad_type_id integer,
+	rad_type_name text,
+
+	primary key (rad_type_id, rad_type_name)
+	);
+
+-- start of misc.
+
+create table grade --of elementary school per year.
+	(
+	ch_id integer,
+	grade integer,
+
+	foreign key (ch_id) references character(ch_id),
+	primary key (ch_id, grade)
+	);
+
+create table stroke_count
+	(
+	ch_id integer,
+	count integer,
+	count_no integer, --if zero the most accurate for the same kanji.
+	
+	foreign key (ch_id) references character(ch_id),
+	primary key (ch_id, count, count_no)
+	);
+
+create table variant --related kanji characters.
+	(
+	ch_id integer,
+	var_value text,
+	var_type_id integer,
+
+	foreign key (ch_id) references character(ch_id),
+	foreign key (var_type_id) references var_type(var_type_id),
+	primary key (ch_id, var_value, var_type_id)
+	);
+
+--/ meta
+create table var_type --name of jis 208 jis 212 etc.
+	(
+	var_type_id integer,
+	vartype_name text,
+
+	primary key (var_type_id, vartype_name)
+	);
+
+create table freq -- frequancy of usage in nwewspapers.
+	(
+	ch_id integer,
+	freq_value integer,
+
+	foreign key (ch_id) references character(ch_id),
+	primary key (ch_id, freq_value)
+	);
+
+create table jlpt -- jlpt test level.
+	(
+	ch_id integer,
+	jlpt_level integer,
+
+	foreign key (ch_id) references character(ch_id),
+	primary key (ch_id, jlpt_level)
+	);
+
+create table rad_name -- name if the kanji itself is used as radical
+	(
+	ch_id integer,
+	rad_name text,
+
+	foreign key (ch_id) references character(ch_id),
+	primary key (ch_id, rad_name)
+	);
+
+-- end of misc
+
+create table dic_ref
+	(
+	ch_id integer,
+	dic_ref text,
+	dr_type_id integer,
+	m_volume integer,
+	m_page integer,
+
+	foreign key (ch_id) references character(ch_id),
+	foreign key (dr_type_id) references dr_type(dr_type_id),	
+	primary key (ch_id, dic_ref, dr_type_id, m_volume)
+	);
+
+-- / meta
+create table dr_type
+	(
+	dr_type_id integer,
+	dr_type_name text,
+
+	primary key (dr_type_id, dr_type_name)
+	);
+
+create table q_code
+	(
+	ch_id integer,
+	q_code text,
+	qc_type_id integer,
+	skip_misclass text,
+
+	foreign key (ch_id) references character(ch_id),
+	foreign key (qc_type_id) references qc_type(qc_type_id),	
+	primary key (ch_id, q_code, qc_type_id)
+	);
+
+-- / meta
+create table qc_type
+	(
+	qc_type_id integer,
+	qc_type_name text,
+
+	primary key (qc_type_id, qc_type_name)
+	);
+
+create table reading
+	(
+	ch_id integer,
+	reading_value text,
+	r_type_id integer,
+	r_status texr,
+
+	foreign key (ch_id) references character(ch_id),
+	foreign key (r_type_id) references r_type(r_type_id),	
+	primary key (ch_id, reading_value, r_type_id)
+	);
+
+-- / meta
+create table r_type
+	(
+	r_type_id integer,
+	r_type_name text,
+
+	primary key (r_type_id, r_type_name)
+	);
+
+create table meaning
+	(
+	ch_id integer,
+	meaning_value text,
+	m_lang_id integer,
+
+	foreign key (ch_id) references character(ch_id),
+	foreign key (m_lang_id) references m_lang(m_lang_id),	
+	primary key (ch_id, meaning_value, m_lang_id)
+	);
+
+-- / meta
+create table m_lang
+	(
+	m_lang_id integer,
+	m_lang_code_value text,
+
+	primary key (m_lang_id, m_lang_code_value)
+	);
+
+create table nanori
+	(
+	ch_id integer,
+	nanori_value text,
+
+	foreign key (ch_id) references character(ch_id)
+	primary key (ch_id, nanori_value)
+	);
+	
+create view kanji_finder as
+
+    select 
+    character.ch_id as id, 
+    character.literal as kanji,
+    
+    cp_value.cp_value as code,
+    cp_type.cp_type_name as code_type,
+    
+    rad_value.rad_value as radical_number,
+    rad_type_name.rad_type_name as radical_type,
+    
+    grade.grade as grade,
+    stroke_count.count as stroke_count,
+    stroke_count.count_no as miscount,
+    
+    variant.var_value as variant_code,
+    var_type.vartype_name as variant_code_type,
+    
+    freq.freq_value as frequancy,
+    jlpt.jlpt_level as jlpt_level,
+    
+    dic_ref.dic_ref as reference_number,
+    dic_ref.m_volume as reference_volume,
+    dic_ref.m_page as reference_page,
+    dr_type.dr_type_name as reference_name, 
+    
+    q_code.q_code as query_code,
+    q_code.skip_misclass as skip_misclass,
+    qc_type.qc_type_name as query_method_name,
+    
+    reading.reading_value as reading,
+    reading.r_status as jouyou_limit,
+    r_type.r_type_name as reading_type,
+    
+    meaining.meaning_value as meaning,
+    m_lang.m_lang_code_value as meaning_lang_code,
+    
+    nanori.nanori_value as nanori_reading,
+    
+    rad_name.rad_name as radical_name 
+    
+    from 
+    character
+    left join cp_value on cp_value.ch_id = character.ch_id
+    left join cp_type on cp_value.cp_type_id = cp_type.cp_type_id 
+    
+    left join rad_value on rad_value.ch_id = character.ch_id
+    left join rad_type on rad_type.rad_type_id = rad_value.rad_type_id 
+    
+    left join grade on grade.ch_id = character.ch_id
+    left join stroke_count on stroke_count.ch_id = c.ch_id
+    
+    left join variant on variant.ch_id = character.ch_id
+    left join var_type on var_type.var_type_id = variant.var_type_id
+    
+    left join freq on freq.ch_id = character.ch_id
+    left join jlpt on jlpt.ch_id = character.ch_id
+    left join rad_name on rad_name.ch_id = character.ch_id
+    
+    left join dic_ref on dic_ref.ch_id = character.ch_id
+    left join dr_type on dr_type.dr_type_id = dic_ref.dr_type_id
+    
+    left join q_code on q_code.ch_id = character.ch_id
+    left join qc_type on qc_type.qc_type_id = q_code.qc_type_id
+
+    left join reading on reading.ch_id = character.ch_id
+    left join r_type on r_type.r_type_id = reading.r_type_id
+    
+    left join meaninig on meaninig.ch_id = character.ch_id
+    left join m_lang on m_lang.m_lang_id = meaninig.m_lang_id
+    
+    left join nanori on nanori.ch_id = character.ch_id
+
+
+
+
+
---
+++ kd2sqlite.py
@@ -0,0 +1,236 @@
+import xml.sax
+import sqlite3
+# 6355_5801_952_13108
+# kanjidic2 header
+#('霓', 38675)
+# 飴
+# dic_ref instead of number and it must converted to text
+# skip_misclass is text q_code
+
+
+class kanjidicHandler(xml.sax.handler.ContentHandler):
+    """class to convert kanjiDic2.xml to sqlite tables  """
+    crntElmnt = ""  # current element.
+    crntAtrs = {}  # current attributes.
+    crntChar = ""   # current kanji character.
+    crntID = 0      # current character id "utf-8 code to decimal"
+    count4ID = 0    # count for strok count occurances in the smae character
+
+    # tables tuples.
+    character = []
+    cp_value = []
+    #
+    cp_type = []
+    rad_value = []
+    #
+    rad_type = []
+    grade = []
+    stroke_count = []
+    variant = []
+    #
+    var_type = []
+    freq = []
+    jlpt = []
+    dic_ref = []
+    #
+    dr_type = []
+    rad_name = []
+    q_code = []
+    #
+    qc_type = []
+    reading = []
+    #
+    r_type = []
+    meaninig = []
+    #
+    m_lang = []
+    nanori = []
+
+
+
+
+
+    def startDocument(self):
+        print("document Startet")
+
+    def startElement(self, name, attrs):
+        self.crntElmnt = name
+        self.crntAtrs = attrs
+        print(attrs.getNames())
+        if name == "stroke_count":
+            self.count4ID += 1
+
+    def endElement(self, name):
+        self.crntElmnt = ""
+        self.crntAtrs = ""
+        if name == "character":
+            self.count4ID = 0
+
+    def characters(self, content):
+
+        if self.crntElmnt == "literal":
+            self.crntChar = content
+            kid = ord(content) # kanji utf-8 to used as id after converting it to decimal integer
+            self.crntID = kid
+            tmpTuple = (kid ,content)
+            self.character.append(tmpTuple)
+
+        elif self.crntElmnt == "cp_value":
+            cp_id = None
+            if self.crntAtrs["cp_type"] not in self.cp_type:
+                self.cp_type.append(self.crntAtrs["cp_type"])
+                cp_id = self.cp_type.index(self.crntAtrs["cp_type"])
+            else:
+                cp_id = self.cp_type.index(self.crntAtrs["cp_type"])
+            tmpTuple = (self.crntID, content, cp_id)
+            self.cp_value.append(tmpTuple)
+
+        elif self.crntElmnt == "rad_value":
+            rad_id = None
+            if self.crntAtrs["rad_type"] not in self.rad_type:
+                self.rad_type.append(self.crntAtrs["rad_type"])
+                rad_id = self.rad_type.index(self.crntAtrs["rad_type"])
+            else:
+                rad_id = self.rad_type.index(self.crntAtrs["rad_type"])
+            tmpTuple = (self.crntID, content, rad_id)
+            self.rad_value.append(tmpTuple)
+
+        elif self.crntElmnt == "grade":
+            tmpTuple = (self.crntID, content)
+            self.grade.append(tmpTuple)
+
+        elif self.crntElmnt == "stroke_count":
+            tmpTuple = (self.crntID, content, self.count4ID)
+            self.stroke_count.append(tmpTuple)
+
+        elif self.crntElmnt == "variant":
+            var_id = None
+            if self.crntAtrs["var_type"] not in self.var_type:
+                self.var_type.append(self.crntAtrs["var_type"])
+                var_id = self.var_type.index(self.crntAtrs["var_type"])
+            else:
+                var_id = self.var_type.index(self.crntAtrs["var_type"])
+            tmpTuple = (self.crntID, content, var_id)
+            self.variant.append(tmpTuple)
+
+        elif self.crntElmnt == "freq":
+            tmpTuple = (self.crntID, content)
+            self.freq.append(tmpTuple)
+
+        elif self.crntElmnt == "rad_name":
+            tmpTuple = (self.crntID, content)
+            self.rad_name.append(tmpTuple)
+
+        elif self.crntElmnt == "jlpt":
+            tmpTuple = (self.crntID, content)
+            self.jlpt.append(tmpTuple)
+
+        elif self.crntElmnt == "dic_ref":
+            m_vol = self.crntAtrs.get("m_vol")
+            m_page = self.crntAtrs.get("m_page")
+            dic_id = None
+            if self.crntAtrs["dr_type"] not in self.dr_type:
+                self.dr_type.append(self.crntAtrs["dr_type"])
+                dic_id = self.dr_type.index(self.crntAtrs["dr_type"])
+            else:
+                dic_id = self.dr_type.index(self.crntAtrs["dr_type"])
+            tmpTuple = (self.crntID, content, dic_id, m_vol, m_page)
+            self.dic_ref.append(tmpTuple)
+
+        elif self.crntElmnt == "q_code":
+            skip_misclass = self.crntAtrs.get("skip_misclass")
+            qc_id = None
+            if self.crntAtrs["qc_type"] not in self.qc_type:
+                self.qc_type.append(self.crntAtrs["qc_type"])
+                qc_id = self.qc_type.index(self.crntAtrs["qc_type"])
+            else:
+                qc_id = self.qc_type.index(self.crntAtrs["qc_type"])
+            tmpTuple = (self.crntID, content,qc_id , skip_misclass)
+            self.q_code.append(tmpTuple)
+
+        elif self.crntElmnt == "reading":
+            r_status = self.crntAtrs.get("r_status")
+            rt_id = None
+            if self.crntAtrs["r_type"] not in self.r_type:
+                self.r_type.append(self.crntAtrs["r_type"])
+                rt_id = self.r_type.index(self.crntAtrs["r_type"])
+            else:
+                rt_id = self.r_type.index(self.crntAtrs["r_type"])
+            tmpTuple = (self.crntID, content, rt_id, r_status)
+            self.reading.append(tmpTuple)
+
+        elif self.crntElmnt == "meaning":
+            lang_id = None
+            lang = self.crntAtrs.get("m_lang", "en")
+            if lang not in self.m_lang:
+                self.m_lang.append(lang)
+                lang_id = self.m_lang.index(lang)
+            else:
+                lang_id = self.m_lang.index(lang)
+            tmpTuple = (self.crntID, content, lang_id)
+            self.meaninig.append(tmpTuple)
+
+        elif self.crntElmnt == "nanori":
+            tmpTuple = (self.crntID, content)
+            self.nanori.append(tmpTuple)
+
+    def endDocument(self):
+        schema_file = open("kanjidic2_schema.sql","r")
+        schema = schema_file.read()
+        print(schema)
+        import datetime
+        timestamp = datetime.datetime.now()
+
+        connection = sqlite3.connect("kanji_dic_sqlite" + timestamp.isoformat("_"))
+        cursor = connection.cursor()
+        cursor.executescript(schema)
+        cursor.executemany("insert into character values (?,?)", self.character)
+
+        cursor.executemany("insert into	cp_type	values (?,?)", enumerate(self.cp_type))
+        cursor.executemany("insert into	cp_value values (?,?,?)", self.cp_value)
+
+        cursor.executemany("insert into	rad_type values (?,?)", enumerate(self.rad_type))
+        cursor.executemany("insert into	rad_value values (?,?,?)", self.rad_value)
+
+        cursor.executemany("insert into	grade values (?,?)", self.grade)
+        cursor.executemany("insert into	stroke_count values (?,?,?)", self.stroke_count)
+
+        cursor.executemany("insert into	var_type values (?,?)", enumerate(self.var_type))
+        cursor.executemany("insert into	variant	values (?,?,?)", self.variant)
+
+        cursor.executemany("insert into	freq	values (?,?)", self.freq)
+        cursor.executemany("insert into	jlpt	values (?,?)", self.jlpt)
+
+        cursor.executemany("insert into	dr_type	values (?,?)", enumerate(self.dr_type))
+        cursor.executemany("insert into	dic_ref	values (?,?,?,?,?)", self.dic_ref)
+
+        cursor.executemany("insert into	rad_name	values (?,?)", self.rad_name)
+
+        cursor.executemany("insert into	qc_type	values (?,?)", enumerate(self.qc_type))
+        cursor.executemany("insert into	q_code	values (?,?,?,?)", self.q_code)
+
+        cursor.executemany("insert into	r_type	values (?,?)", enumerate(self.r_type))
+        cursor.executemany("insert into	reading	values (?,?,?,?)", self.reading)
+
+        cursor.executemany("insert into	m_lang	values (?,?)", enumerate(self.m_lang))
+        cursor.executemany("insert into	meaning	values (?,?,?)", self.meaninig)
+
+        cursor.executemany("insert into	nanori	values (?,?)", self.nanori)
+
+
+        connection.commit()
+        print("Done.")
+
+
+
+
+
+
+handler = kanjidicHandler()
+
+
+import xml.sax
+ff = open("kanjidic2.xml","r")
+xml.sax.make_parser()
+xml.sax.parse(ff, handler)
+
